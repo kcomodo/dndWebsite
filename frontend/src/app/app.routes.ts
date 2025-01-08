f4c07d08ed2044587a5120759bad053f16babd8e
Joseph R <josephruffennach3@gmail.com>
2025-01-08 15:12:40 -0700

    Sign-in Component changes: added login component to front end added global background to styles.css using font "Inter" completed login ui

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/components/home/home.component';
+import { LoginComponent } from '../app/components/login/login.component';
 export const routes: Routes = [
   {
     path: 'home',
     component:HomeComponent
+  },
+  {
+    path: 'login',
+    component:LoginComponent
   },
   {
     path: '',
