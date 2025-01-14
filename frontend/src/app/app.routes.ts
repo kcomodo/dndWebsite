cd26f11ae94f9441eef2b97cf7ba06ba615ce800
Quang Ho <mxrshmalloww@gmail.com>
2025-01-14 14:55:38 -0500

    NodeJs Generated basic crud test connection successful to DB, server startup is successful

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/components/home/home.component';
 import { LoginComponent } from '../app/components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
 export const routes: Routes = [
   {
     path: 'home',
@@ -14,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'signup',
     component:SignupComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   },
   {
     path: '',
