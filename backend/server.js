eeacc62842e69ff4ffb95f1e67ea0ca6f29142cd
CatExposure <andrenewbauer@gmail.com>
2025-01-20 21:47:08 -0500

    added .env variables, connectivity to database and basic query to ensure functionality

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,54 @@
 'use strict';
 
-var http = require('http');
-var express = require('express');
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 1337;
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const mysql = require('mysql2');
+const port = process.env.PORT || 1337;
+
+const con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.ROOT_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+con.connect(function (err) {
+    if (err) {console.log(err)}
+    else {console.log("Connected!")};
+});
+
+function test() {
+    con.query('SELECT * FROM userInfo', function (err, result) {
+        if (err) { console.log(err)
+        } else {console.log(result)}});
+}
+
+test()
+
+const corsOptions = {
+    origin: "https:localhost:4200",
+    optionsSucessStatus: 204,
+    credentials: true,
+};
 
 // Create an Express app
-var app = express();
+const app = express();
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
-
-// Import user routes from the routes folder
-var userRoutes = require('./routes/user');
-
 // Use user routes for any endpoint starting with '/api'
-app.use('/api', userRoutes);
+app.use(cors());
+
+//using this to handle preflight requests so that they also intake the corsOptions
+app.options('*', cors(corsOptions));
+
+app.get("/login", cors(corsOptions), async function(){
+
+})
 
 // Create and run the HTTP server
 http.createServer(app).listen(port, function () {
