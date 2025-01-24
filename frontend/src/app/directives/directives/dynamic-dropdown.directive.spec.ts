24706d190a6aa1fc2c024cf850182c86e574e169
Joseph R <josephruffennach3@gmail.com>
2025-01-24 16:24:25 -0700

    deleted:    frontend/public/homepage_background.png 	modified:   frontend/src/app/components/character/character.component.css 	modified:   frontend/src/app/components/character/character.component.html 	modified:   frontend/src/app/components/character/character.component.ts 	new file:   frontend/src/app/directives/directives/dynamic-dropdown.directive.spec.ts 	new file:   frontend/src/app/directives/directives/dynamic-dropdown.directive.ts

---
+++ frontend/src/app/directives/directives/dynamic-dropdown.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DynamicDropdownDirective } from './dynamic-dropdown.directive';
+
+describe('DynamicDropdownDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DynamicDropdownDirective();
+    expect(directive).toBeTruthy();
+  });
+});
