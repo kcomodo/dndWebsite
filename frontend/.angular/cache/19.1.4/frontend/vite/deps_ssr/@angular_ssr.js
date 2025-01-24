f15fea945aeaa9e886907d15e059c5af33638b9a
Quang Ho <mxrshmalloww@gmail.com>
2025-01-24 12:32:55 -0500

    Landing page Fixed width of page, and worked on carousel.

---
+++ frontend/.angular/cache/19.1.4/frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,37 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  setAngularAppEngineManifest,
+  setAngularAppManifest
+} from "./chunk-EBWYG4YT.js";
+import "./chunk-VZZ44ABQ.js";
+import "./chunk-2XOJEFQJ.js";
+import "./chunk-V47UZLHH.js";
+import "./chunk-IQR53VJN.js";
+import "./chunk-S7EJFDHQ.js";
+import "./chunk-KJBLNTLG.js";
+import "./chunk-FPO3XRGI.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
