7e4bfa9a664eb29f465d978c9c15319edc581cb3
CatExposure <andrenewbauer@gmail.com>
2025-01-27 14:19:34 -0500

    added some comments for clarity as well as ended connection to DBMS when request was finished

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,7 @@ const mysql = require('mysql2');
 const crypto = require('crypto');
 const port = process.env.PORT || 1337;
 
-console.log(process.env.HOST);
-const con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.ROOT_USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
-
-con.connect(function (err) {
-    if (err) {console.log(err)}
-    else {console.log("Connected!")};
-});
-
+//allows cookies to be passed with http requests as well as restricts specific domains to access the server
 const corsOptions = {
     origin: "https:localhost:4200",
     optionsSucessStatus: 204,
@@ -38,6 +26,30 @@ app.use(cors());
 //using this to handle preflight requests so that they also intake the corsOptions
 app.options('*', cors(corsOptions));
 
+//this function is called everytime a connection to the database needs to be opened
+//this is wrapped in a promise to allow us to await the connection to finish before executing any further code
+function createConnection() {
+    return new Promise((resolve, reject) => {
+        try {
+            const con = mysql.createConnection({
+                host: process.env.HOST,
+                user: process.env.ROOT_USER,
+                password: process.env.PASSWORD,
+                database: process.env.DATABASE
+            });
+
+            con.connect(function (err) {
+                if (err) {console.log(err)}
+                else {console.log("connection created!"); resolve(con);};
+            });
+        } catch (error) {
+            reject(error);
+        }   
+    });
+}
+
+//this function is called whenever a query is to be executed in our dbms
+//wrapped in a promise to ensure we can await the results before executing further code
 function executeQuery(query, params) {
     return new Promise((resolve, reject) => {
         con.execute(query, params, (err, result, fields) => {
@@ -47,6 +59,7 @@ function executeQuery(query, params) {
     });
 }
 
+//updates the tokens expiration date
 async function updateExpireDate(clientEmail) {
     const newDate = new Date(new Date().setDate(new Date().getDate() + 7));
     const query = "UPDATE userInfo SET authExpire = ? WHERE userEmail = ?";
@@ -54,6 +67,7 @@ async function updateExpireDate(clientEmail) {
     await executeQuery(query, params);
 }
 
+//obtains a token that allows the user to access the site without having to login for 7 days
 async function obtainToken(clientEmail) {
     const token = crypto.randomBytes(16).toString('hex');
     const query = "UPDATE userInfo SET authCode = ? WHERE userEmail = ?";
@@ -63,6 +77,7 @@ async function obtainToken(clientEmail) {
     return token;
 }
 
+//authorizes the token to ensure it's the same token. This helps with ensuring the data is the clients, not somebody else using the clients credentials on their own behalf
 async function authorizeToken(clientEmail) {
     const query = "SELECT authCode, authExpire FROM userInfo WHERE userEmail = ?";
     const params = [clientEmail];
@@ -78,13 +93,20 @@ async function authorizeToken(clientEmail) {
         }
 }
 
-function validatePW(clientPW, passwordSalt, userPW) {
+//hashes the clients password which we use to both insert into the database as well as cross check to ensure the password they entered is the same on the DB
+async function hashPW(clientPW, passwordSalt) {
     var hash = crypto.createHash('sha256');
     hash.update(clientPW);
     var pwHash = hash.digest('hex');
     hash = crypto.createHash('sha256');
-    hash.update(clientPW+passwordSalt+clientPW);
+    hash.update(pwHash+passwordSalt+clientPW);
     pwHash = hash.digest('hex');
+    return pwHash;
+}
+
+//cross checks the password between the clients entered password and the password on the DB
+async function validatePW(clientPW, passwordSalt, userPW) {
+    const pwHash = await hashPW(clientPW, passwordSalt)
     if (pwHash == userPW) {
         return true;
     }
@@ -92,24 +114,28 @@ function validatePW(clientPW, passwordSalt, userPW) {
 }
 
 function test() {
-    con.query(`SELECT * FROM userInfo WHERE user_id = 1`, function (err, result) {
-        if (err) {console.log(err)}
-        else {
-            console.log(result[0].userPassword.toString());
-        }
-        });
+    server.getConnections(function(error, count) {
+        console.log(count);
+    })
 }
 
-app.post("/login", cors(corsOptions), function(req, res){
+app.post("/test", cors(corsOptions), async function(req, res){server.getConnections(function(error, count) {
+    console.log(count);
+})})
+
+app.post("/login", cors(corsOptions), async function(req, res){
     console.log(req.body);
     const clientEmail = req.body.clientEmail;
     const clientPW = req.body.clientPW;
 
+    con = await createConnection();
     con.execute("SELECT * FROM userInfo WHERE userEmail = ?", [clientEmail], async function (err, result) {
-        if (err) {res.send("Email is invalid")}
+        if (err) {console.log(err)}
+        //if the array length is falsey, then the query returned no data matching that email
+        else if (!result.length) {con.end(); res.send("Email is invalid"); return;}
 
-        if (!validatePW(clientPW, result[0].passwordSalt, result[0].userPassword.toString())) {
-            res.send("Password is invalid");
+        else if (!validatePW(clientPW, result[0].passwordSalt, result[0].userPassword.toString())) {
+            con.end(); res.send("Password is invalid"); return;
         }   
 
         const token = await authorizeToken(clientEmail);
@@ -120,25 +146,31 @@ app.post("/login", cors(corsOptions), function(req, res){
                 userName: result[0].username,
                 authCode: token
             };
+            con.end();
             res.send(userData)
         };
     });
 })
 
-app.post("/newLogin", cors(corsOptions), function(req, res) {
+app.post("/newLogin", cors(corsOptions), async function(req, res) {
     console.log(req);
     const clientUsername = req.body.clientUsername;
     const clientEmail = req.body.clientEmail;
     const clientPW = req.body.clientPW;
-
-    con.query("INSERT INTO userInfo (username, userPassword, userEmail) VALUES (?, ?, ?)", [clientUsername, clientPW, clientEmail], async function (err, result) {
-        if (err) {console.log(err); if (err.errno === 1062) {res.send("Email already in use")}}
+    
+    con = await createConnection();
+    
+    const passwordSalt = crypto.randomBytes(16).toString('hex');
+    const hashedPW = await hashPW(clientPW, passwordSalt);
+    con.execute("INSERT INTO userInfo (username, userPassword, userEmail, passwordSalt) VALUES (?, ?, ?, ?)", [clientUsername, hashedPW, clientEmail, passwordSalt], async function (err) {
+        if (err) {console.log(err); if (err.errno === 1062) {con.end(); res.send("Email already in use")}}
         else {
+            con.end();
             res.send(true);
         }
     })
 })
 // Create and run the HTTP server
-http.createServer(app).listen(port, function () {
+const server = http.createServer(app).listen(port, function () {
     console.log(`Server running on http://localhost:${port}`);
 });
